fce4f3167604c17fcca9363f299289dd72fe7748
doug <linderd@iinet.net.au>
2014-05-20 14:38:21 +0800

    cleanup

--- dist/core/stuff.d.ts
+++
@@ -1,1 +0,0 @@
-/// <reference path="../node_modules/xq/xq.d.ts" />
--- dist/xq/xq.d.ts
+++
@@ -1,9 +0,0 @@
-/// <reference path="../../src/xq/defs/orm/orm.d.ts" />
-/// <reference path="../../src/xq/defs/q/Q.d.ts" />
-import orm = require('orm');
-import base = require('./base');
-export declare class Base<T> extends base.Base<T> {
-}
-export interface Def extends base.Def {
-}
-export declare var connect: (uri: string) => Q.Promise<orm.ORM>;
